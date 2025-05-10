1631ce1da093ed512f4b7aba7e9fe7e48ccc264b
Flatolitou <115586273+Flatolitou@users.noreply.github.com>
2025-05-10 18:46:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "spring-2025-openings",
-    title: "Spring 2025 Openings Party Rank Sorter",
-    description: "Party rank sorter for Spring 2025 Openings."
+    localStoragePrefix: "spring-2025-endings",
+    title: "Spring 2025 Endings Party Rank Sorter",
+    description: "Party rank sorter for Spring 2025 Endings."
 };
